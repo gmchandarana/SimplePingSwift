9fb8dda50e0b61b9ee46a6d36697c6d13e704f98
Gaurav Chandarana <gmchandarana@gmail.com>
2024-04-18 12:19:33 +0100

    Initial commit.

---
+++ SimplePingSwift/SimplePingSwift.h
@@ -0,0 +1,18 @@
+//
+//  SimplePingSwift.h
+//  SimplePingSwift
+//
+//  Created by Gaurav Chandarana on 18/04/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimplePingSwift.
+FOUNDATION_EXPORT double SimplePingSwiftVersionNumber;
+
+//! Project version string for SimplePingSwift.
+FOUNDATION_EXPORT const unsigned char SimplePingSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimplePingSwift/PublicHeader.h>
+
+
