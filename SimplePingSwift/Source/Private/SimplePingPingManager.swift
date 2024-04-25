6f1954f0a1e5d724e4d3d41f9200bbef037193cf
Gaurav Chandarana <gmchandarana@gmail.com>
2024-04-25 15:06:38 +0100

    Refactor SimplePingPingManager.

--- SimplePingSwift/Source/Private/SimplePingPingManager.swift
+++ SimplePingSwift/Source/Private/SimplePingPingManager.swift
@@ -7,32 +7,36 @@
 
 import Foundation
 
-public class SimplePingPingManager: PingManager {
+class SimplePingPingManager: PingManager {
 
     private var requests = [UInt16: Date]()
     private var results = [UInt16: TimeInterval]()
     private var session: PingSession?
+    private var responseHandler: PingResponseHandler?
 
     func ping(host: String, configuration: PingConfiguration, _ responseHandler: PingResponseHandler? = nil, _ resultHandler: PingResultHandler? = nil) {
-        
+
+        self.responseHandler = responseHandler
         let session = PingSession(host: host, pingInterval: configuration.interval)
 
         session.start { [weak self] response in
-
             guard let self else { return }
-            switch response {
-            case .sent(_, let packet, _):
-                self.requests.updateValue(Date(), forKey: packet)
-            case .received(_, let packet, let host):
-                guard let start = requests[packet] else { return }
-                let elapsed = abs(start.timeIntervalSinceNow)
-                self.results.updateValue(elapsed, forKey: packet)
-                responseHandler?(.success(PingSuccess(host: host, time: elapsed)))
-            case .failed(let error, _):
-                responseHandler?(.failure(error))
-            default: break
-            }
+            self.handle(response)
         }
         self.session = session
+    }
+
+    private func handle(_ response: PingSessionResponse) {
+        switch response {
+        case .sent(_, let packet, _): requests.updateValue(Date(), forKey: packet)
+
+        case .received(_, let packet, let host):
+            guard let start = requests[packet] else { return }
+            let elapsed = abs(start.timeIntervalSinceNow)
+            results.updateValue(elapsed, forKey: packet)
+            responseHandler?(.success(PingSuccess(host: host, time: elapsed)))
+        case .failed(let error, _): responseHandler?(.failure(error))
+        default: break
+        }
     }
 }
