28274c455eb97895f1393ca15c59b005a60fabc1
Gaurav Chandarana <gmchandarana@gmail.com>
2024-04-25 11:15:26 +0100

    Implement SimplePingPingManager.

---
+++ SimplePingSwift/Source/Private/SimplePingPingManager.swift
@@ -0,0 +1,15 @@
+//
+//  SimplePingPingManager.swift
+//  SimplePingSwift
+//
+//  Created by Gaurav Chandarana on 25/04/2024.
+//
+
+import Foundation
+
+class SimplePingPingManager: PingManager {
+
+    func ping(host: String, configuration: PingConfiguration, responseHandler: (Result<Void, any Error>) -> (), resultHandler: (Result<Void, any Error>) -> ()) {
+
+    }
+}
